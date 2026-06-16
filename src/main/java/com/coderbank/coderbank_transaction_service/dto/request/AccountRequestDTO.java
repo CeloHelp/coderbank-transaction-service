4b46b38b700dd7ce8c13bfbb382e32635d2f9b9b
Marcelo <marcelopacobello@gmail.com>
2026-06-15 21:46:49 -0300

    feat: criando os DTOs de request e response da entidade Account

--- src/main/java/com/coderbank/coderbank_transaction_service/dto/request/AccountRequestDTO.java
+++ src/main/java/com/coderbank/coderbank_transaction_service/dto/request/AccountRequestDTO.java
@@ -1,6 +1,26 @@
 package com.coderbank.coderbank_transaction_service.dto.request;
 
+import com.coderbank.coderbank_transaction_service.model.CurrencyType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import java.math.BigDecimal;
+
 public record AccountRequestDTO(
+        @NotBlank(message = "Customer ID é obrigatório")
         String customerId,
-        String accountType) {
+
+        @NotNull(message = "Valor Inicial é obrigatório")
+        @PositiveOrZero(message = "Valor Inicial deve ser positivo ou zero")
+        BigDecimal amount,
+
+        @NotNull(message = "A moeda é  obrigatório")
+        CurrencyType currency,
+        
+        @NotNull(message = "Descrição é obrigatória")
+        String description // ignoramos aqui?
+
+
+) {
 }
--- src/main/java/com/coderbank/coderbank_transaction_service/dto/response/AccountResponseDTO.java
+++ src/main/java/com/coderbank/coderbank_transaction_service/dto/response/AccountResponseDTO.java
@@ -1,4 +1,16 @@
 package com.coderbank.coderbank_transaction_service.dto.response;
 
-public class AccountResponseDTO {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record AccountResponseDTO (
+        UUID accountId,
+
+        String customerId,
+
+        BigDecimal balance,
+
+        LocalDateTime createdAt
+){
 }
